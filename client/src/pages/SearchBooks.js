76ebb6736f0d4b01cbaaaeb67e191060cdb62758
Matt Lahey <mdlahey@outlook.com>
2023-03-08 18:04:58 -0500

    updated Client side and fixed bugs in Server side

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+
+import { SAVE_BOOK } from '../utils/mutations';
+
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -63,19 +68,23 @@ const SearchBooks = () => {
     if (!token) {
       return false;
     }
-
-    try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+    console.log(bookToSave)
+    const response = await saveBook({
+      variables: {
+        authors: bookToSave.authors,
+        description: bookToSave.description,
+        title: bookToSave.title,
+        bookId: bookToSave.bookId,
+        image: bookToSave.image,
+        link: bookToSave.link
       }
+    });
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
+
+
+    // if book successfully saves to user's account, save book id to state
+    setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+
   };
 
   return (
--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,6 +1,11 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
+const typeDefs = gql`  
+  type User {
+    username: String! 
+    email: String!
+    savedBooks: [Book]
+  }
   type Book {
     bookId: String
     authors: [String]
@@ -9,32 +14,17 @@ const typeDefs = gql`
     image: String
     link: String
   }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
-  input bookInput {
-    bookId: String
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
+  type Auth {
+    token: ID!
+    user: User
   }
   type Query {
     me: User
   }
-  type Auth {
-    token: ID
-    user: User
-  }
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
+    saveBook(authors: [String], description: String!, title: String!, bookId: String, image: String, link: String): User
     removeBook(bookId: String!): User
   }
 `;
